64e40a49aad7d12834e1f147137bd8a9cffaac89
Austin Corwin <austin.corwin@brewlogix.com>
2024-04-09 10:15:46 -0600

    Removed evolved hybrid training

--- src/app/_config/pages/homepageConfig.ts
+++ src/app/_config/pages/homepageConfig.ts
@@ -97,10 +97,6 @@ export const homepageConfig = {
         subtitle: 'Explore our Evolved training programs!',
         items: [
             {
-                title: 'Evolved Hybrid Training ',
-                body: `Let us do the heavy lifting for you! Experience a transformative 8-day program with our expert trainers. Your dog will receive 8 full days with our team followed by 8 private sessions covering advanced obedience, including off-leash reliability, behavioral modification for issues like aggression, anxiety, and public manners. Automatic access to Evolve For Life, where you will receive lessons for life and unlimited support!`,
-            },
-            {
                 title: 'Evolved Private Training',
                 body: `Jump into a personalized training journey, where we not only transform your dog but YOU as well! With 12 private lessons, covering basic and advanced obedience, including off-leash reliability and behavioral modification for ALL issues including aggression and anxiety. Our program includes Relationship Repair, where we help guide you on your journey to redefining your relationship with your dog. Automatic access to Evolve For Life, where you will receive lessons for life and unlimited support!`,
             },
